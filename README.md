fe3179b1ba975e2d49ca59caf76109b136da7d40
carlos velasquez <ceduardorubio@hotmail.com>
2023-05-20 23:15:12 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,164 @@
+# Web Socket Node Server 
+
+## Description
+Transform your express / http application server into a websocket server to handle websocket connections, broadcast and receive messages and send response to the received messages from websocket clients.
+    - No redis required
+    - No database required
+    - No Socket.io required
+    - Auto reconnection 
+
+USE THIS LIBRARY WITH 
+
+- [web socket server side client](https://www.npmjs.com/package/ws-server-side-client) to connect another server side application to the Web Socket Node Server.
+
+- [web socket browser client](https://www.npmjs.com/package/ws-browser-client) to connect a browser application to the Web Socket Node Server.
+
+
+## Installation
+```bash
+npm install --save ws-node-server
+```
+## 1. Setup
+Instantiate the WebSocketNodeServer class with the http server instance.
+```typescript
+import { WebSocketNodeServer } from "ws-node-server";
+import http                    from 'http';
+import express                 from 'express';
+const app    = express();
+const server = http.createServer(app);
+// app middleware, routes, etc
+// ...
+const wsServer = new WebSocketNodeServer(server);
+```
+## 2. Listeners Setup
+```typescript
+let wsOnAuthReq = (credential:any, setSession:(sessionData,groups) => void, response:(error: any, clientSideSessionObject: { [key: string]: any}) => void) => {
+    // validate credentials like username and password
+    if(credential.user == "..." && credential.password == "..."){
+        let currentToken = GenerateToken();
+        AddTokenToDatabase(currentToken);
+        // set the session data
+        let sessionData = {
+            user   : credential.user,
+            isAdmin: true,
+            token  : currentToken
+        }
+        // set the groups
+        let groups = ['admin'];
+        // set the session
+        setSession(sessionData,groups);
+        // send the response
+        response(null,sessionData);
+    } else {
+        // send the response
+        response('invalid credentials',null);
+    }
+}
+let wsOnConnClose = (sessionData:any) => {
+    // remove the token from the database
+    RemoveTokenFromDatabase(sessionData.token);
+}
+let wsOnHTTPUpgradeRequest = (req:IncomingMessage,allow: () => void,deny:() => void) => {
+
+    let ipHeader  = request.headers['cf-connecting-ip'] || request.headers['ipHeader-forwarded-for'] || request.connection.remoteAddress;
+    let isBanned = IsBannedIP(ipHeader);
+    if(isBanned){
+        deny();
+    } else {
+        allow();
+    }
+
+}
+
+// set the listeners
+// OnAuthentication: listen when a client sends an authentication request
+wsServer.OnAuthentication(wsOnAuthReq);
+// OnHTTPUpgradeRequest: listen when a client sends an http upgrade request (starts the websocket connection)
+wsServer.OnHTTPUpgradeRequest(wsOnHTTPUpgradeRequest);
+// OnConnectionClose: listen when a client closes the connection
+wsServer.OnConnectionClose(wsOnConnClose);
+```
+### 2.1 OnAuthentication
+Set this listener as a function to handle the incoming authentication requests from the clients. It receives the next parameters: 
+- **credentials:** the credential object send by the client, like username and password, token, etc.
+
+- **setSession**  set the session data and groups to the client if the authentication is valid. It receives the next parameters:
+    - **sessionData:** used to set the session data to the client. It can be any object.
+    - **groups:** used to set the array of strings with the groups to be set to the client.
+
+
+### 2.2 OnHTTPUpgradeRequest
+Set this listener as a function to handle the incoming http upgrade requests from the clients. It receives the next parameters:
+- **req:** the http request object.
+- **allow:** call this function to allow the connection.
+- **deny:** call this function to deny the connection.
+
+### 2.3 OnConnectionClose
+Set this listener as a function to handle the incoming connection close requests from the clients. It receives the next parameters:
+- **sessionData:** the session data object send  of the client that has closed the connection.
+
+
+
+## 3. Connection Setup
+```typescript
+wsServer.StartListening();
+```
+
+## 4. Set Request Handlers
+```typescript
+wsServers.OnRequest('create/user',(requestBody,response,sessionData,clientGroups) => {
+    let user = requestBody;
+    let allowCreate = sessionData.isAdmin || clientGroups.includes('admin');
+    if(allowCreate){
+        // create user in database
+        response(null,{done:true});
+    } else {
+        response('not allowed',null);
+    }
+});
+```
+### 4.1 OnRequest
+Set this listener as a function to handle the incoming requests from the clients. It receives the next parameters:
+- **requestName:** the name of the request to listen.
+- **requestBody:** the request body object send by the client.
+- **response:** call this function to send the response to the client. It receives the next parameters:
+    - **error:** the error object to be send to the client.
+    - **data:** the data object to be send to the client.
+- **sessionData:** the session data object of the client that has sent the request.
+- **clientGroups:** the array of strings with the groups names of the client that has sent the request.
+
+
+## 5. Send Broadcast Messages
+```typescript
+// only to admin group members
+wsServers.Broadcast('test',"admin",{yourMessage:'only to admin group members'});
+// to everyone
+wsServers.Broadcast('test', null,{yourMessage:'to everyone'});
+```
+### 5.1 Broadcast
+Send a broadcast message to all the clients or to a specific group. It receives the next parameters:
+- **eventName:** the name of the event to send.
+- **groupName:** the name of the group to send the message. If null, the message will be send to all the clients.
+- **data:** the data object to be send to the clients.
+
+
+## READ THE CODE ON
+
+[github: websocket-node-server](https://github.com/ceduardorubio/websocket-node-server)
+
+## License
+
+[MIT](LICENSE)
+
+## Author
+
+Carlos Velasquez - [ceduardorubio](https://github.com/ceduardorubio)
+
+## Keywords
+
+[websocket](https://www.npmjs.com/search?q=keywords:web%20socket), [websocket server](https://www.npmjs.com/search?q=keywords:websocket%server), [websocket node server](https://www.npmjs.com/search?q=keywords:websocket%node%20server)
+
+## Change Log
+
+### 0.0.1
+- Initial release
---
+++ src/index.ts
@@ -0,0 +1,247 @@
+import { IncomingMessage, Server } from 'http';
+import internal = require('stream');
+import { WebSocketServer,WebSocket,OPEN } from 'ws';
+
+interface SocketSession {
+    isAlive: boolean,
+    data   : { [key: string]: any},
+    groups : string[],
+}
+
+interface SocketPackageInfo {
+    action   : 'group' | 'call' | 'auth' | 'broadcast',
+    request  : string,
+    group    : string,
+    packageID: number
+}
+
+interface SocketPackage {
+    info: SocketPackageInfo,
+    data: { [key: string]: any}
+}
+
+interface SocketPackageResponse {
+    info    : SocketPackageInfo,
+    error   : any,
+    response: { [key: string]: any}
+}
+
+
+interface SocketRouter {
+    [key:string]:(
+        requestBody:{ [key: string]: any},
+        response:(error: any, response: { [key: string]: any}) => void,
+        sessionData:{ [key: string]: any},
+        clientGroups:string[]
+    ) => void
+}
+
+export type AuthLoginFn              = (
+    data:any,
+    setSession:(sessionData:{ [key: string]: any},clientGroups:string[])=> void,
+    authResponse:(error: any, clientSideSessionObject: { [key: string]: any}) => void
+    ) => void
+
+export class WebSocketNodeServer {
+    public  websocketServer : WebSocketServer             = new WebSocketServer({ noServer: true });
+    private onSocketError   : (err:Error) => void         = (err:Error) => { console.log(err); };
+    private server          : Server | null               = null;
+    private routes          : SocketRouter                = {};
+    private broadcastPackageID:number                     = 0;
+    private heartbeatInterval: number                     = 9000;
+    private authTimeout: number                           = 6000;
+    private onCloseClientConnection:(session:any) => void = null;
+    private httpRequestHandler: (request:IncomingMessage, allow:()=>void, deny:()=>void) => void = null;
+    private onAuthReq:AuthLoginFn                         = (
+        data:any,
+        setSession:(sessionData:{ [key: string]: any},
+        clientGroups:string[])=> void,
+        authResponse:(error: any, clientSideSessionObject: { [key: string]: any}) => void) => {
+            authResponse('No authentication function defined',null);
+        };  
+    
+    constructor(server:Server){
+        this.server = server;
+    }
+    
+    OnAuthentication(onAuthentication:(
+        credentialsObject:{ [key: string]: any},
+        setSession:(sessionData:{ [key: string]: any},clientGroups:string[])=> void,
+        authResponse:(error: any, clientSideSessionObject: { [key: string]: any}) => void
+        ) => void){
+        this.onAuthReq = onAuthentication;
+    }
+
+    OnRequest (requestName:string,fn:(
+            requestBody:{ [key: string]: any},
+            response:(error: any, response: { [key: string]: any}) => void,
+            sessionData:{ [key: string]: any},
+            clientGroups:string[]
+        ) => void) {
+        this.routes[requestName] = fn;
+        return this;
+    }
+
+    OnConnectionClose(onCloseClientConnection:(session:any) => void ){
+        this.onCloseClientConnection = onCloseClientConnection;
+        return this;
+    }
+
+    OnHTTPUpgradeRequest (handler :(request:IncomingMessage, allows:()=>void, deny:()=>void) => void){
+        this.httpRequestHandler = handler;
+        return this;
+    }
+
+    public StartListening (){
+        this.server.on('upgrade',  (request:IncomingMessage, socketInternal:internal.Duplex, head:Buffer) => {
+
+            socketInternal.on('error',this.onSocketError);
+            socketInternal.removeListener('error', this.onSocketError);
+
+            const deny = () => socketInternal.destroy(); 
+            const allow    = () => {
+                this.websocketServer.handleUpgrade(request, socketInternal, head,(websocket:WebSocket) => {
+                    this.websocketServer.emit('connection',request,websocket,socketInternal)
+                });
+            }
+            if ( this.httpRequestHandler ) {
+                this.httpRequestHandler(request ,allow,deny);
+            } else {
+                allow();
+            }  
+        });
+
+        this.websocketServer.on('connection',(httpRequest:IncomingMessage,websocket:WebSocket, socketInternal:internal.Duplex) => {
+            const session:SocketSession = {
+                isAlive: true,  // heartbeat
+                data   : null,  // session data 
+                groups : [],    // groups
+            }
+            websocket    ['xSession'] = session;                                                      // add session to websocket
+            const        closeSocketInternal = () => socketInternal.destroy();                        // close socket internal (at http level)
+            setTimeout   (() => (session.data !== null) || closeSocketInternal() ,this.authTimeout);  // client has N seconds to authenticate or will be disconnected
+            websocket.on('error', this.onSocketError);                                                // error handler
+            websocket.on('pong', () => session.isAlive = true );                                      // heartbeat set isAlive 
+            websocket.on('close', () => { 
+                if(session.data && this.onCloseClientConnection) this.onCloseClientConnection(session.data);           // if the client was authenticated, call the onCloseClientConnection callback
+            });
+            websocket.on('message', (message : string) => {
+                let { 
+                    info, // info about the package, so the package can be routed in server and the response can be tracked when it arrives to the client 
+                    data  // data sent by the client
+                } = JSON.parse(message) as SocketPackage;
+                let { 
+                    action ,  // internal action builtIn to be performed by the server. Example: group, call, auth, broadcast
+                    request,  // request or route to be performed by the server. Example: join, leave, echo, login, logout, etc
+                    group  ,  // group to be joined or leaved
+                } = info;
+    
+                const SendToClient= (error:any,response:{ [key: string]: any} ) => { // function definition to send response to client. Info is the same as was sent by the client
+                    let res: SocketPackageResponse = { info, error, response };
+                    websocket.send(JSON.stringify(res))
+                }
+    
+                if (session.data){                                          // if the client is authenticated
+                   if(action === 'group'){
+                        if(request === 'join'){                             // join a group
+                            session.groups.push(group);
+                            SendToClient(false,{done:true});
+                        } else {
+                            if(request === 'leave'){                        // leave a group
+                                session.groups = session.groups.filter(g => g !== group);
+                                SendToClient(false,{done:true});
+                            } else {
+                                if(request === 'leaveAll'){                 // leave all groups
+                                    session.groups = [];
+                                    SendToClient(false,{done:true});
+                                } else {
+                                    SendToClient('invalid group request',{ done : false }); // invalid group request
+                                }
+                            }
+                        }
+                    } else {
+                        if(action == 'call'){
+                            if(request == 'echo') {                         // built in echo. Sends back the data sent by the client. Test the connection 
+                                SendToClient(false,{ echoAt: new Date().getTime(), received:data});
+                            } else {
+                                if(Object.keys(request).length > 0){     // if there are routes defined
+                                    if(request in this.routes){                   // if the route exists
+                                        this.routes[request](data,SendToClient,session.data,session.groups); // call the route
+                                    } else {
+                                        SendToClient('invalid call request',{ done : false });
+                                    }
+                                } else {                                   // if there are no routes defined
+                                    SendToClient('invalid call. No routes defined',{ done : false });
+                                }
+                            }
+                        } else {
+                            SendToClient('invalid action',{ done : false });
+                        }
+                    }
+                } else { // if the client is not authenticated
+                    if((action === 'auth') && (request === 'login')){ // if the client is trying to authenticate
+                        const startSession = (sessionData,groups) => {
+                            session.data = sessionData || {};
+                            session.groups = groups || [];
+                        }
+                        this.onAuthReq(data,startSession,SendToClient); // call the authentication function
+                    } else {
+                        // if the client is not authenticated and is not trying to authenticate, close the socket
+                        closeSocketInternal();                        
+                    }                     
+                }
+            });
+        });
+    
+        const heartBeating = setInterval(() => {
+            this.websocketServer.clients.forEach(ws => {
+                if     (ws['xSession'] === undefined) return ws.terminate();       // if the client is not authenticated, close the socket
+                if     (ws['xSession'].isAlive  === false) return ws.terminate();  // if the client is not responding to the heartbeat, close the socket
+                ws     ['xSession'].isAlive  = false;                              // set the client as not responding to the heartbeat and wait for the next heartbeat
+                ws.ping();                                                         // send the heartbeat
+            });
+        }, this.heartbeatInterval);
+    
+        this.websocketServer.on('close', () => clearInterval(heartBeating));
+        return this;
+    }
+        
+    Broadcast (eventName:string,groupName:string | null,data:{ [key: string]: any},emitter:WebSocket = null) {
+        this.broadcastPackageID++;
+        let info: SocketPackageInfo = {  action: 'broadcast', request: eventName, group:groupName, packageID:this.broadcastPackageID  };
+        let r : SocketPackageResponse = { info, error: false, response: data };
+        let msg = JSON.stringify(r);
+        this.websocketServer.clients.forEach((ws:WebSocket) => {
+            if (ws.readyState === OPEN && ("xSession" in ws) && (ws !== emitter)) {
+                if(groupName){
+                    if(ws['xSession']['groups'].includes(groupName))  ws.send( msg);
+                } else {
+                    ws.send( msg);
+                }                
+            }
+        });
+        return this;
+    };
+
+    Close () {
+        this.websocketServer.close();
+        return this;
+    };
+
+    SetHeartbeatInterval (time:number) {
+        this.heartbeatInterval = time;
+        if(this.authTimeout > time){
+            this.authTimeout = time * 0.75;
+        }
+        return this;
+    }
+
+    SetTimeoutForAuthenticationRequest (time:number) {
+        this.authTimeout = time;
+        if(this.heartbeatInterval < time){
+            this.heartbeatInterval = time * 1.5;
+        }
+        return this;
+    }
+
+}
